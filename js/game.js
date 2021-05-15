b974232637ad84b59e9bec5874cc1b4a5bc9bbbc
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-05-15 13:54:37 -0600

    Added very, very basic leveling system.

--- js/game.js
+++ js/game.js
@@ -1,11 +1,9 @@
+'use strict';
+
 import {speech} from './tts';
 import Timer from './timer';
-import $ from 'jquery';
-import {utils} from './utilities';
-import {ScrollingText} from './scrollingText';
 import {KeyboardInput} from './input.js';
 import {KeyEvent} from './keycodes.js';
-import {so} from './soundObject';
 
 class Game {
   constructor() {
@@ -15,27 +13,18 @@ class Game {
       update: () => this.update(),
       render: () => this.render()
     }, 1 / 60);
+    this.level = 1;
   }
 
   start() {
-    this.sound = so.create('menu_music');
-    this.sound.loop = true;
-    this.sound.play();
     this.timer.start();
   }
 
   update(dt) {
-    if (this.input.isJustPressed(KeyEvent.DOM_VK_UP)) {
-      speech.speak('Pitch up.');
-      this.sound.playbackRate += 0.01;
+    if (this.input.isJustPressed(KeyEvent.DOM_VK_L)) {
+      speech.speak('Level ' + this.level + '.');
     }
-    if (this.input.isJustReleased(KeyEvent.DOM_VK_UP)) {
-    }
-    if (this.input.isJustPressed(KeyEvent.DOM_VK_DOWN)) {
-      speech.speak('Pitch down.');
-      this.sound.playbackRate -= 0.01;
-    }
-    if (this.input.isJustReleased(KeyEvent.DOM_VK_DOWN)) {
+    if (this.input.isJustReleased(KeyEvent.DOM_VK_L)) {
     }
   }
 
